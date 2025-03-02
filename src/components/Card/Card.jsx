036b769a379adfa855af3d23820c65de066c9672
GlebShylovich <shylovichgleb@gmail.com>
2025-03-02 10:51:48 +0100

    modal component

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,14 +1,18 @@
+import { useState } from "react";
+import Modal from "../Modal/Modal";
 import "./Card.scss"
 
 export default function Card({ data }) {
   const { name, img, price, weight } = data;
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <div className="card">
+    <div className="card" onClick={() => setIsOpen(true)}>
       <img className="card__image" src={img} alt={name} />
       <h3 className="card__price">{price}$</h3>
       <p className="card__name">{name}</p>
       <p className="card__weight">{weight}g</p>
       <button className="card__button">Add</button>
+      {isOpen && <Modal data={data} closeModal={setIsOpen}/>}
     </div>
   );
 }
---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import close from "/close.svg";
+import "./Modal.scss";
+
+export default function Modal({ data, closeModal }) {
+  const { name, img, desc, ingredients, weight, calories, price } = data;
+  const [count, setCount] = useState(1);
+  
+  return (
+    <div className="modal__overlay">
+      <div className="modal" onClick={(e) => e.stopPropagation()}>
+        <div className="modal__top">
+          <h2 className="modal__title">{name}</h2>
+          <button
+            className="modal__close-button"
+            onClick={() => closeModal(false)}
+          >
+            <img src={close} alt="close" />
+          </button>
+        </div>
+        <div className="modal__body">
+          <img src={img} alt={name} className="modal__image" />
+          <div className="modal__info">
+            <div className="modal__info-description">{desc}</div>
+            <div className="modal__info-composition">
+              <h4 className="modal__info-composition-title">Ingridients:</h4>
+              {Object.values(ingredients).map((item, index) => (
+                <p key={index} className="modal__info-composition-item">
+                  {item}
+                </p>
+              ))}
+              <p className="modal__info-composition-info">
+                {weight}g, ccal {calories}
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="modal__controls">
+          <button className="modal__button">Add</button>
+          <div className="modal__controls-info">
+            <div className="modal__counter">
+              <button onClick={() => count > 1 && setCount(count - 1)}>
+                -
+              </button>
+              <p>{count}</p>
+              <button onClick={() => setCount(count + 1)}>+</button>
+            </div>
+            <p className="modal__price">{(price * count).toFixed(2)}$</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
